347b8ba549f622337659ce21079f0840450d36c6
Niyazov0607 <shohjahonniyazov89@gmail.com>
2024-12-30 23:43:13 +0500

    practice

---
+++ script.js
@@ -0,0 +1,39 @@
+// let fruits = ["apple", "banana", "cherry"];
+// let result = fruits.join(", ");
+// console.log(result); 
+
+
+// let students = {Anvar: 18, Shox: 18, Siddiq: 19};
+// console.log(Object.keys(students).length);
+
+
+// let products = {apple: 2000, banana: 3000, cherry: 1000};
+// const value = Object.values(products).reduce((acc, price) => acc + price, 0);
+// console.log(value);
+
+
+// function person(name) {
+//     this.name = name;
+// }
+
+// person.prototype.speak = function () {
+//     console.log(`hey my name is ${this.name}`);
+// }
+// const person1 = new person("Shohjahon");
+// person1.speak();
+
+// const person2 = new person("Laylo");
+// person2.speak();
+
+
+
+// let str = "hello";
+// console.log(str.toUpperCase());
+
+// let str = [1, 2, 3];
+// str.push(4);
+// console.log(str);
+
+
+// let mass = [2, 4, 5, 6];
+// console.log(mass.length);
